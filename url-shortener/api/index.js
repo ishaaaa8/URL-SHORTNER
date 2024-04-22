782f52c29746cf746e09195b7b9ed7c94b5f3b87
ishaaaa8 <itsisha4884@gmail.com>
2024-04-22 19:32:43 +0530

    Connected to mongoDB

--- url-shortener/api/index.js
+++ url-shortener/api/index.js
@@ -1,6 +1,9 @@
 const express=require("express");
 const app=express();
 const PORT = 8001;
+const urlRoute = require('./routes/url');
+
+app.use('/url',urlRoute);
 
 app.listen(PORT, () => console.log(`Server at Port 8001`));
 
--- url-shortener/api/routes/url.js
+++ url-shortener/api/routes/url.js
@@ -1,4 +1,8 @@
 const express=require('express');
 const router=require.Router();
+const {handleGenerateNewShortURL} = require("../controllers/url");
 
-router.post('/');
+router.post('/',handleGenerateNewShortURL);
+
+
+module.exports=router;
